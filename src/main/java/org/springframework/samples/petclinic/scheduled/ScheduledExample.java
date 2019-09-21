fdc3af647f678e015980c2ad497fc3ac87aa9d67
Roman Nemchinov <nemchinovrp@yandex.ru>
2019-09-21 17:11:06 +0300

    disable scheduling in project

--- src/main/java/org/springframework/samples/petclinic/scheduled/ScheduledExample.java
+++ src/main/java/org/springframework/samples/petclinic/scheduled/ScheduledExample.java
@@ -1,17 +1,26 @@
 package org.springframework.samples.petclinic.scheduled;
 
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-@Component
+//@Component
 public class ScheduledExample {
+    /**
+     * Fixed Delay scheduler is used to execute the tasks at a specific time.
+     * It should wait for the previous task completion.
+     * The values should be in milliseconds.
+     */
     @Scheduled(fixedDelay = 5000)
     public void fixedDelayDemo() {
         System.out.println("Method fixedDelayDemo() executed at every 5 seconds. Current time is :: " + new Date());
     }
 
+    /**
+     * Fixed Rate scheduler is used to execute the tasks at the specific time.
+     * It does not wait for the completion of previous task.
+     * The values should be in milliseconds
+     */
     @Scheduled(fixedRate = 5000)
     public void fixedRateDemo() {
         System.out.println("Method fixedRateDemo() executed at every 5 seconds. Current time is :: " + new Date());
