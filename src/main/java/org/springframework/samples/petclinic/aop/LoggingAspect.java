d67bb9bd0fe908e15364bc9f5d1801e6bb4b8de6
Roman Nemchinov <nemchinovrp@yandex.ru>
2019-09-23 14:23:26 +0300

    fixed aspect

--- src/main/java/org/springframework/samples/petclinic/aop/LoggingAspect.java
+++ src/main/java/org/springframework/samples/petclinic/aop/LoggingAspect.java
@@ -17,6 +17,11 @@ public class LoggingAspect {
     @After("execution(* org.springframework.samples.petclinic.aop.EmployeeManagerImpl.getEmployeeById(..))")
     public void logAfterGetEmployee(JoinPoint joinPoint) {
         System.out.println("****LoggingAspect.logAfterGetEmployee() : " + joinPoint.getSignature().getName());
+        System.out.println("joinPoint.getKind() - " + joinPoint.getKind().toString());
+        System.out.println("joinPoint.getTarget() - " + joinPoint.getTarget().toString());
+        System.out.println("joinPoint.getThis() - " + joinPoint.getThis().toString());
+        System.out.println("joinPoint.toLongString() - " + joinPoint.toLongString());
+        System.out.println("joinPoint.toShortString() - " + joinPoint.toShortString());
     }
 
     @After("execution(* org.springframework.samples.petclinic.aop.EmployeeManagerImpl.createEmployee(..))")
--- src/main/java/org/springframework/samples/petclinic/controller/TestAop.java
+++ src/main/java/org/springframework/samples/petclinic/controller/TestAop.java
@@ -1,7 +1,6 @@
 package org.springframework.samples.petclinic.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.aop.EmployeeDTO;
 import org.springframework.samples.petclinic.aop.EmployeeManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +13,5 @@ public class TestAop {
     @GetMapping("/aop")
     public void testAop() throws Exception {
         manager.getEmployeeById(1);
-        manager.createEmployee(new EmployeeDTO());
     }
 }
