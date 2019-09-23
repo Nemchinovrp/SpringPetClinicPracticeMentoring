97d471517f8f4b992336fac89291db9d9d243776
Roman Nemchinov <nemchinovrp@yandex.ru>
2019-09-23 09:24:06 +0300

    added async in project

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -1,21 +1,64 @@
 package org.springframework.samples.petclinic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.samples.petclinic.async.GitHubLookupService;
+import org.springframework.samples.petclinic.model.User;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * PetClinic Spring Boot Application.
  *
  * @author Dave Syer
- *
  */
-@EnableScheduling
+@EnableAsync
 @SpringBootApplication
-public class PetClinicApplication {
+public class PetClinicApplication implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(PetClinicApplication.class);
+
+    @Autowired
+    private GitHubLookupService gitHubLookupService;
+
+    @Bean("threadPoolTaskExecutor")
+    public TaskExecutor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(20);
+        executor.setMaxPoolSize(1000);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setThreadNamePrefix("Async-");
+        return executor;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PetClinicApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        // Start the clock
+        long start = System.currentTimeMillis();
+        // Kick of multiple, asynchronous lookups
+        CompletableFuture<User> page1 = gitHubLookupService.findUser("PivotalSoftware");
+        CompletableFuture<User> page2 = gitHubLookupService.findUser("CloudFoundry");
+        CompletableFuture<User> page3 = gitHubLookupService.findUser("Spring-Projects");
+        CompletableFuture<User> page4 = gitHubLookupService.findUser("RameshMF");
+        // Wait until they are all done
+        CompletableFuture.allOf(page1, page2, page3, page4).join();
+        // Print results, including elapsed time
+        logger.info("Elapsed time: " + (System.currentTimeMillis() - start));
+        logger.info("--> " + page1.get());
+        logger.info("--> " + page2.get());
+        logger.info("--> " + page3.get());
+        logger.info("--> " + page4.get());
+    }
 }
